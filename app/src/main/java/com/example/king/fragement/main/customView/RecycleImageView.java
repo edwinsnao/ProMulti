0a8d387439bde20abe6080dc9910d49338f6f54e
‘u013571833’ <448517683@qq.com>
2016-12-01 19:28:53 +0800

    enhance:完善了recycleImageView

--- app/src/main/java/com/example/king/fragement/main/customView/RecycleImageView.java
+++ app/src/main/java/com/example/king/fragement/main/customView/RecycleImageView.java
@@ -1,7 +1,29 @@
-public class RecycleImageView extends ImageView{
-	onDetachFromWindow(){
-		imageloader.cancelRequest
-//				setImageBitmap(null);
+package com.example.king.fragement.main.customView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.example.king.fragement.main.BaseApplication;
+
+public class RecycleImageView extends ImageView {
+	public RecycleImageView(Context context) {
+		super(context);
+	}
+
+	public RecycleImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public RecycleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		BaseApplication.getLoader().cancelDisplayTask(this);
+		setImageBitmap(null);
 		setImageDrawable(null);
 	}
 }
